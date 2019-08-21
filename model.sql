e54c30abacede26ceb976fb5dfb0f7a307567505
JanuaryZeng <15841331413@163.com>
2019-08-21 19:52:54 +0800

    修改

--- model.sql
+++ model.sql
@@ -4,19 +4,22 @@ use fruit;
 create table Users(
 Uname varchar(25) primary key,
 Upassword varchar(25) not null,
-ovenCount varchar(4)
+ovenCount varchar(4),
+Ucontext varchar(50),
+phone varchar(12)
 );
 
 create table Ovens(
 id int NOT NULL AUTO_INCREMENT,
-bakeCount varchar(4),
+Oname varchar(10),
 statue varchar(4),
 primary key (id)
 );
 
 create table Fruits(
 Fname varchar(25) primary key,
-mean varchar(255)
+mean varchar(255),
+Ficon varchar(100)
 );
 
 create table Notes(
@@ -25,9 +28,11 @@ id int,
 Fname varchar(25),
 startTime varchar(10),
 endTime varchar(10),
+Uname varchar(25),
 primary key (noteId),
 foreign key(id) references Ovens(id),
-foreign key(Fname) references Fruits(Fname)
+foreign key(Fname) references Fruits(Fname),
+foreign key(Uname) references USers(Uname)
 
 );
 
@@ -37,6 +42,7 @@ noteId int,
 temp varchar(8),
 humi varchar(8),
 Ltime varchar(10),
+Lev varchar(4),
 primary key(id),
 foreign key(noteId) references Notes(noteId)
 );
